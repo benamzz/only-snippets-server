e637273094faa213c627545ff95dc72fc471fef9
Alan ROGER <aa.roger404@gmail.com>
2022-10-03 17:26:50 +0200

    clean code routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,10 +4,27 @@ const User = require("../models/User.model");
 const Snippet = require("../models/Snippet.model");
 const mongoose = require("mongoose");
 
+//détails user
+router.get("/users/:userId", (req, res, next) => {
+  User.findById(req.params.userId)
+    .then(response => res.status(200).json({ user: response }))
+    .catch(err => next(err));
+});
+
+//édition user
+router.patch("/users/:userId", (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+  User.findByIdAndUpdate(req.params.userId, req.body, { new: true })
+    .then(response => res.status(200).json({ userUpdated: response }))
+    .catch(err => next(err));
+});
+
+//création article
 router.post("/articles", (req, res, next) => {
   const { parentId } = req.query;
-
-  console.log("HELLO REQ = ", req.payload._id);
   Article.create({
     content: req.body.content,
     tag: req.body.tag,
@@ -16,196 +33,119 @@ router.post("/articles", (req, res, next) => {
   })
     .then(function (articleFromDB) {
       console.log("Article created : ", articleFromDB);
-
-      // Article.findById(parentId)
-      //   .then(parentArticleFromDB => {
-      //     parentArticleFromDB.comments.push(articleFromDB._id)
-      //     parentArticleFromDB.save()
-      //       .then()
-      //       .catch()
-      //   })
-      //   .catch()
-
       Article.findByIdAndUpdate(parentId, {
-        $push:{comments:articleFromDB._id.toString()},
+        $push: { comments: articleFromDB._id.toString() },
       })
-        .then(function () {
-          res.status(201).json(articleFromDB); // 201 Created
-        })
-        .catch(function (err) {
-          // console.log("Error creating comment : ", err);
-          next(err);
-        });
+        // la ligne ci dessus est équivalente à:
+        // Article.findById(parentId)
+        //   .then(parentArticleFromDB => {
+        //     parentArticleFromDB.comments.push(articleFromDB._id)
+        //     parentArticleFromDB.save()
+        //       .then()
+        //       .catch()
+        //   })
+        //   .catch()
+        .then(() => res.status(201).json(articleFromDB))
+        .catch(err => next(err));
     })
-    .catch(function (err) {
-      // console.log("Error creating article : ", err);
-      next(err); // middleware d'erreur
-    });
+    .catch(err => next(err));
 });
 
-router.get("/articles/:id", (req, res, next) => {
-  console.log("req.params : ", req.params);
-  Article.findById(req.params.id)
-    .then((response) => {
-      console.log("article = ", response);
-      res.status(200).json({ article: response });
-    })
-    .catch((error) => console.log("Error finding article : ", error));
+//détails article
+router.get("/articles/:articleId", (req, res, next) => {
+  Article.findById(req.params.articleId)
+    .then(response => res.status(200).json({ article: response }))
+    .catch(err => next(err))
 });
 
+//liste des articles
 router.get("/articles", (req, res, next) => {
   Article.find()
-    .then((response) => {
-      console.log("articles = ", response);
-      res.status(200).json({ articles: response });
-    })
-    .catch((error) => console.log("Error finding articles : ", error));
+    .then(response => res.status(200).json({ articles: response }))
+    .catch(err => next(err))
 });
 
-router.patch("/articles/:id", (req, res, next) => {
-  console.log("req.params : ", req.params);
-
+//édition article
+router.patch("/articles/:articleId", (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
-
-  Article.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then((response) => {
-      console.log("article = ", response);
-      res.status(200).json({ articleUpdated: response });
-    })
-    .catch((error) => console.log("Error updating article : ", error));
+  Article.findByIdAndUpdate(req.params.articleId, req.body, { new: true })
+    .then(response => res.status(200).json({ articleUpdated: response }))
+    .catch(err => next(err))
 });
 
-router.get("/users/:id", (req, res, next) => {
-  console.log("req.params : ", req.params);
-  User.findById(req.params.id)
-    .then((response) => {
-      console.log("article = ", response);
-      res.status(200).json({ user: response });
-    })
-    .catch((error) => console.log("Error finding user : ", error));
-});
-
-router.patch("/users/:id", (req, res, next) => {
-  console.log("req.params : ", req.params);
-
-  if (!mongoose.Types.ObjectId.isValid(req.params)) {
-    res.status(400).json({ message: "Specified id is not valid" });
-    return;
-  }
-
-  User.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then((response) => {
-      console.log("user = ", response);
-      res.status(200).json({ userUpdated: response });
-    })
-    .catch((error) => console.log("Error updating user : ", error));
-});
-
-router.post("/articles/:id/snippets", (req, res, next) => {
-  console.log("PostId = ", req.params.id);
+//création snippet
+router.post("/articles/:articleId/snippets", (req, res, next) => {
   Snippet.create({
     content: req.body.content,
     userId: req.payload._id,
     articleId: req.params.id,
   })
-    .then(function (snippetFromDB) {
-      console.log("Snippet created : ", snippetFromDB);
-      res.status(201).json(snippetFromDB); // 201 Created
-    })
-    .catch(function (err) {
-      console.log("Error creating snippet : ", err);
-      next(err); // middleware d'erreur
-    });
+    .then(snippetFromDB=>res.status(201).json(snippetFromDB))
+    .catch(err => next(err))
 });
 
+//édition snippet
 router.patch("/articles/:articleId/snippets/:snippetId", (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.snippetId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
-
   Snippet.findByIdAndUpdate(req.params.snippetId, req.body, { new: true })
-    .then((response) => {
-      console.log("snippet = ", response);
-      res.status(200).json({ snippetUpdated: response });
-    })
-    .catch((error) => console.log("Error updating snippet : ", error));
+    .then(response => res.status(200).json({ snippetUpdated: response }))
+    .catch(err => next(err))
 });
 
+//détail snippet
 router.get("/articles/:articleId/snippets/:snippetId", (req, res, next) => {
   Snippet.findById(req.params.snippetId)
-    .then((response) => {
-      console.log("snippet = ", response);
-      res.status(200).json({ snippet: response });
-    })
-    .catch((error) => console.log("Error finding snippet : ", error));
+    .then(response => res.status(200).json({ snippet: response }))
+    .catch(err => next(err))
 });
 
 // suppression snippet
 router.delete("/articles/:articleId/snippets/:snippetId", (req, res, next) => {
   Snippet.findByIdAndRemove(req.params.snippetId)
-    .then((response) => {
-      // console.log("snippet = ", response)
-      res.status(204).send();
-    })
-    .catch((error) => console.log("Error removing snippet : ", error));
+    .then(()=>res.status(204).send())
+    .catch(err => next(err))
 });
 
 // suppression article
 router.delete("/articles/:articleId", (req, res, next) => {
   Article.findByIdAndRemove(req.params.articleId)
-    .then((response) => {
-      res.status(204).send();
-    })
-    .catch((error) => console.log("Error removing article : ", error));
+    .then(() => res.status(204).send())
+    .catch(err => next(err))
 });
 
-//LIKES
+//like d'un article
 router.put("/articles/:articleId/like", (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.articleId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
-
-  User.findByIdAndUpdate(
-    req.payload._id,
-    { likes: req.params.articleId },
-    { new: true }
-  )
-    .then((response) => {
-      res.status(200).json({ userUpdated: response });
-    })
-    .catch((error) => console.log("Error updating user's likes : ", error));
+  User.findByIdAndUpdate(req.payload._id,{likes: req.params.articleId},{new: true})
+    .then(response => res.status(200).json({ userUpdated: response }))
+    .catch(err => next(err))
 });
 
-// followers
+// follow d'un user
 router.put("/users/:userId/follow", (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.userId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
-
-  User.findByIdAndUpdate(
-    req.payload._id,
-    { following: req.params.userId },
-    { new: true }
-  )
-    .then((response) => {
-      res.status(200).json({ userUpdatedFollows: response });
-    })
-    .catch((error) => console.log("Error updating user's follows : ", error));
+  User.findByIdAndUpdate(req.payload._id,{following: req.params.userId},{new: true})
+    .then(response => res.status(200).json({ userUpdatedFollows: response }))
+    .catch(err => next(err))
 });
 
+//liste des commentaires relatifs a un article
 router.get("/articles/:articleId/comments", (req, res, next) => {
   Article.find({ parentId: req.params.articleId })
-    .then((response) => {
-      console.log("comments = ", response);
-      res.status(200).json({ comments: response });
-    })
-    .catch((error) => console.log("Error finding comments : ", error));
+    .then(response => res.status(200).json({ comments: response }))
+    .catch(err => next(err))
 });
 
 module.exports = router;
