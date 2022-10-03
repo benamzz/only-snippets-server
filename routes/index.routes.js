f1dd26997b9e0377dd50d3720ac400da3280ab4b
Alan ROGER <aa.roger404@gmail.com>
2022-10-03 18:06:16 +0200

    ajout du isAuthenticated sur les routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,16 +3,17 @@ const Article = require("../models/Article.model");
 const User = require("../models/User.model");
 const Snippet = require("../models/Snippet.model");
 const mongoose = require("mongoose");
+const { isAuthenticated } = require('./../middleware/jwt.middleware.js');
 
 //détails user
-router.get("/users/:userId", (req, res, next) => {
+router.get("/users/:userId", isAuthenticated,(req, res, next) => {
   User.findById(req.params.userId)
     .then(response => res.status(200).json({ user: response }))
     .catch(err => next(err));
 });
 
 //édition user
-router.patch("/users/:userId", (req, res, next) => {
+router.patch("/users/:userId", isAuthenticated,(req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
@@ -23,7 +24,7 @@ router.patch("/users/:userId", (req, res, next) => {
 });
 
 //création article
-router.post("/articles", (req, res, next) => {
+router.post("/articles", isAuthenticated,(req, res, next) => {
   const { parentId } = req.query;
   Article.create({
     content: req.body.content,
@@ -31,12 +32,14 @@ router.post("/articles", (req, res, next) => {
     userId: req.payload._id,
     parentId: parentId,
   })
-    .then(function (articleFromDB) {
-      console.log("Article created : ", articleFromDB);
-      Article.findByIdAndUpdate(parentId, {
-        $push: { comments: articleFromDB._id.toString() },
-      })
-        // la ligne ci dessus est équivalente à:
+    .then((articleFromDB=>{Article.findByIdAndUpdate(parentId, {$push: { comments: articleFromDB._id.toString() }})
+        .then(() => res.status(201).json(articleFromDB))
+        .catch(err => next(err));
+    }))
+    .catch(err => next(err))
+})
+
+// les lignes 35-40 reviennent à faire:
         // Article.findById(parentId)
         //   .then(parentArticleFromDB => {
         //     parentArticleFromDB.comments.push(articleFromDB._id)
@@ -45,28 +48,23 @@ router.post("/articles", (req, res, next) => {
         //       .catch()
         //   })
         //   .catch()
-        .then(() => res.status(201).json(articleFromDB))
-        .catch(err => next(err));
-    })
-    .catch(err => next(err));
-});
 
 //détails article
-router.get("/articles/:articleId", (req, res, next) => {
+router.get("/articles/:articleId",isAuthenticated, (req, res, next) => {
   Article.findById(req.params.articleId)
     .then(response => res.status(200).json({ article: response }))
     .catch(err => next(err))
 });
 
 //liste des articles
-router.get("/articles", (req, res, next) => {
+router.get("/articles", isAuthenticated,(req, res, next) => {
   Article.find()
     .then(response => res.status(200).json({ articles: response }))
     .catch(err => next(err))
 });
 
 //édition article
-router.patch("/articles/:articleId", (req, res, next) => {
+router.patch("/articles/:articleId", isAuthenticated,(req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
@@ -77,7 +75,7 @@ router.patch("/articles/:articleId", (req, res, next) => {
 });
 
 //création snippet
-router.post("/articles/:articleId/snippets", (req, res, next) => {
+router.post("/articles/:articleId/snippets",isAuthenticated, (req, res, next) => {
   Snippet.create({
     content: req.body.content,
     userId: req.payload._id,
@@ -88,7 +86,7 @@ router.post("/articles/:articleId/snippets", (req, res, next) => {
 });
 
 //édition snippet
-router.patch("/articles/:articleId/snippets/:snippetId", (req, res, next) => {
+router.patch("/articles/:articleId/snippets/:snippetId", isAuthenticated,(req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.snippetId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
@@ -99,28 +97,28 @@ router.patch("/articles/:articleId/snippets/:snippetId", (req, res, next) => {
 });
 
 //détail snippet
-router.get("/articles/:articleId/snippets/:snippetId", (req, res, next) => {
+router.get("/articles/:articleId/snippets/:snippetId", isAuthenticated,(req, res, next) => {
   Snippet.findById(req.params.snippetId)
     .then(response => res.status(200).json({ snippet: response }))
     .catch(err => next(err))
 });
 
 // suppression snippet
-router.delete("/articles/:articleId/snippets/:snippetId", (req, res, next) => {
+router.delete("/articles/:articleId/snippets/:snippetId",isAuthenticated, (req, res, next) => {
   Snippet.findByIdAndRemove(req.params.snippetId)
     .then(()=>res.status(204).send())
     .catch(err => next(err))
 });
 
 // suppression article
-router.delete("/articles/:articleId", (req, res, next) => {
+router.delete("/articles/:articleId", isAuthenticated,(req, res, next) => {
   Article.findByIdAndRemove(req.params.articleId)
     .then(() => res.status(204).send())
     .catch(err => next(err))
 });
 
 //like d'un article
-router.put("/articles/:articleId/like", (req, res, next) => {
+router.put("/articles/:articleId/like", isAuthenticated,(req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.articleId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
@@ -131,7 +129,7 @@ router.put("/articles/:articleId/like", (req, res, next) => {
 });
 
 // follow d'un user
-router.put("/users/:userId/follow", (req, res, next) => {
+router.put("/users/:userId/follow", isAuthenticated,(req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.userId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
@@ -142,7 +140,7 @@ router.put("/users/:userId/follow", (req, res, next) => {
 });
 
 //liste des commentaires relatifs a un article
-router.get("/articles/:articleId/comments", (req, res, next) => {
+router.get("/articles/:articleId/comments", isAuthenticated,(req, res, next) => {
   Article.find({ parentId: req.params.articleId })
     .then(response => res.status(200).json({ comments: response }))
     .catch(err => next(err))
