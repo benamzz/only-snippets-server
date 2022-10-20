a8ad226ffa49e3ed6b0319c34158ec9b6bc16a34
Alan ROGER <aa.roger404@gmail.com>
2022-10-20 17:33:22 +0200

    cleaner

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -6,21 +6,18 @@ const { expressjwt: jwt } = require("express-jwt");
 const isAuthenticated = jwt({
   secret: "0n1y5nipp3t",
   algorithms: ["HS256"],
-  requestProperty: 'payload', 
+  requestProperty: 'payload',
   getToken: getTokenFromHeaders
 });
 
 // Function used to extracts the JWT token from the request's 'Authorization' Headers
-function getTokenFromHeaders (req) {
-  
+function getTokenFromHeaders(req) {
   // Check if the token is available on the request Headers
   if (req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer") {
-
     // Get the encoded token string and return it
     const token = req.headers.authorization.split(" ")[1];
     return token;
-  } 
-  
+  }
   return null;
 }
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,28 +9,23 @@ const saltRounds = 10;
 // Création d'un user
 router.post('/users', (req, res, next) => {
   const { email, password } = req.body;
-
   // Check if email or password or name are provided as empty string 
   if (email === '' || password === '') {
     res.status(400).json({ message: "Provide email and password" });
     return;
   }
-
   // Use regex to validate the email format
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
   if (!emailRegex.test(email)) {
     res.status(400).json({ message: 'Provide a valid email address.' });
     return;
   }
-
   // Use regex to validate the password format
   const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
   if (!passwordRegex.test(password)) {
     res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' });
     return;
   }
-
-
   // Check the users collection if a user with the same email already exists
   User.findOne({ email })
     .then((foundUser) => {
@@ -39,11 +34,9 @@ router.post('/users', (req, res, next) => {
         res.status(400).json({ message: "User already exists." });
         return;
       }
-
       // If email is unique, proceed to hash the password
       const salt = bcrypt.genSaltSync(saltRounds);
       const hashedPassword = bcrypt.hashSync(password, salt);
-
       // Create the new user in the database
       // We return a pending promise, which allows us to chain another `then` 
       return User.create({ email, password: hashedPassword });
@@ -52,10 +45,8 @@ router.post('/users', (req, res, next) => {
       // Deconstruct the newly created user object to omit the password
       // We should never expose passwords publicly
       const { email, _id } = createdUser;
-
       //create custom username
       const username = email.split("@")[0] + "#" + _id.toString().slice(-5)
-
       // Create a new object that doesn't expose the password
       const user = { email, _id, username };
       // Send a json response containing the user object
@@ -76,47 +67,38 @@ router.post('/users', (req, res, next) => {
 //Login
 router.post('/sessions', (req, res, next) => {
   const { email, password } = req.body;
-
   // Check if email or password are provided as empty string 
   if (email === '' || password === '') {
     res.status(400).json({ message: "Provide email and password." });
     return;
   }
-
   // Check the users collection if a user with the same email exists
   User.findOne({ email })
     .then((foundUser) => {
-
       if (!foundUser) {
         // If the user is not found, send an error response
         res.status(401).json({ message: "User not found." })
         return;
       }
-
       // Compare the provided password with the one saved in the database
       const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
-
       if (passwordCorrect) {
         // Deconstruct the user object to omit the password
         const { _id, email, avatarUrl, username, bio, location, tags, website, linkedin, github, following, likes } = foundUser;
-
         // Create an object that will be set as the token payload
         const payload = { _id, email, avatarUrl, username, bio, location, tags, website, linkedin, github, following, likes };
-
         // Create and sign the token
         const authToken = jwt.sign(
           payload,
           process.env.TOKEN_SECRET,
           { algorithm: 'HS256', expiresIn: "6h" }
         );
-
         // Send the token as the response
         res.status(200).json({ authToken: authToken });
       }
       else {
         res.status(401).json({ message: "Unable to authenticate the user" });
       }
-
     })
     .catch(err => res.status(500).json({ message: "Internal Server Error" }));
 });
